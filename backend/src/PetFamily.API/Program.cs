381371e0d50b34f0dc37dac3eed9e2aaeda8cd33
Aitishechnik <aitishechka666@gmail.com>
2025-06-10 12:44:56 +0300

    ExceptionMiddleware class and Serolog lib added

--- backend/src/PetFamily.API/Program.cs
+++ backend/src/PetFamily.API/Program.cs
@@ -1,16 +1,34 @@
+using PetFamily.API.Middlewares;
 using PetFamily.Application;
 using PetFamily.Infrastructure;
+using Serilog;
+using Serilog.Events;
 
 var builder = WebApplication.CreateBuilder(args);
 
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.Debug()
+    .WriteTo.Seq(builder.Configuration.GetConnectionString("Seq")
+                 ?? throw new ArgumentNullException("No Seq in ConnectionStrings"))
+    .MinimumLevel.Override("Microsoft.AspNetCore.Hosting", LogEventLevel.Warning)
+    .MinimumLevel.Override("Microsoft.AspNetCore.Mvc", LogEventLevel.Warning)
+    .MinimumLevel.Override("Microsoft.AspNetCore.Rounting", LogEventLevel.Warning)
+    .CreateLogger();
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSerilog();
 
 builder.Services
     .AddInfrastructure()
     .AddApplication();
 
 var app = builder.Build();
+
+app.UseExceptionMiddleware();
+
+app.UseSerilogRequestLogging();
 
 app.MapControllers();
 
